138f1cd5f2baed906409852752e5ee36b9d736db
Tristan Mauer <raykoten@gmail.com>
2023-05-16 16:14:31 +0200

    ajout page erreur 404

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import Home from './pages/Home';
 import Apropos from './pages/Apropos';
-import Header from './components/Header'
+import Header from './components/Header';
+import Error from './components/Error';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,6 +15,7 @@ root.render(
     <Routes>
       <Route path="/" element={<Home />}/>
       <Route path="/Apropos" element={<Apropos />}/>
+      <Route path="*" element={<Error />}/>
     </Routes>
     
     
